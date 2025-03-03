37870420700e7e5133aa6382afc35b1069401e5c
Drishti Saraf <106866070+drishti1920@users.noreply.github.com>
2025-03-03 11:47:43 +0530

    Create Navbar.jsx

---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.styles.css";
+import { CircleX, Menu } from "lucide-react";
+
+const Navbar = () => {
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        <a href="#" className="logo">
+          Logo
+        </a>
+      </Link>
+
+      <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
+      <CircleX className="menu-icon close-icon" size={32} onClick={toggleMenu} />
+        <li>
+          <Link
+            to="/"
+            className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/photography"
+            className={`nav-link ${
+              location.pathname === "/blogs" ? "active" : ""
+            }`}
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Photography
+          </Link>
+        </li>
+        
+
+        <li>
+          <Link
+            to="/film"
+            className={`nav-link ${
+              location.pathname === "/learning-path" ? "active" : ""
+            }`}
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Film
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="/contact"
+            className={`nav-link ${
+              location.pathname === "/learning-path" ? "active" : ""
+            }`}
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Contact
+          </Link>
+        </li>
+
+        <button>Get In Touch</button>
+      </ul>
+      <Menu className="menu-icon" size={32} onClick={toggleMenu} />
+    </nav>
+  );
+};
+
+export default Navbar;
